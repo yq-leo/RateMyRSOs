fe5c953b9001285cd1c05b5eec3f36cfbd0d87da
qiyu6 <qiyu6@illinois.edu>
2024-01-05 04:28:29 -0600

    feat: create homepage ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,34 @@
 import styles from "./page.module.css";
 
+import { FcSearch } from "react-icons/fc";
+import TrendingRSOs from "@/app/ui/home/trending-rsos";
+
 export default function Home() {
-  return <main className={styles.main}>RateMyRSOs</main>;
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>RateMyRSOs</h1>
+      <p className={styles.description}>
+        Rate your favorite RSOs at{" "}
+        <span className={styles.uiucOrange}>UIUC</span>
+      </p>
+      <HeroSearchBar />
+      <TrendingRSOs />
+    </main>
+  );
+}
+
+function HeroSearchBar() {
+  return (
+    <div className={styles.searchBarWrapper}>
+      <div className={styles.searchBar}>
+        <input
+          className={styles.searchInput}
+          type="text"
+          placeholder="Search for an RSO"
+        />
+        <FcSearch className={styles.searchIcon} />
+        <div className={styles.searchBtn}>Search</div>
+      </div>
+    </div>
+  );
 }
