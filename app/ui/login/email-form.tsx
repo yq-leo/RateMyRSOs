520f7b53a1922c44560374391fd2fdfa11bbaebe
qiyu6 <qiyu6@illinois.edu>
2024-01-11 23:07:12 -0600

    feat: change navbar rendering logic

--- app/ui/login/email-form.tsx
+++ app/ui/login/email-form.tsx
@@ -3,8 +3,9 @@
 import { useState, FormEvent, ChangeEvent } from "react";
 import Link from "next/link";
 import styles from "./email-form.module.css";
-import { signInUser } from "@/app/lib/login";
+import { signIn } from "next-auth/react";
 import { SignInFormData, SignInFormError } from "@/app/lib/definitions";
+import { useRouter } from "next/navigation";
 
 export default function EmailForm() {
   const [formData, setFormData] = useState<SignInFormData>({
@@ -12,6 +13,36 @@ export default function EmailForm() {
     password: "",
   });
   const [error, setError] = useState<SignInFormError | null>(null);
+  const router = useRouter();
+
+  const signInUser = async (
+    formData: SignInFormData
+  ): Promise<SignInFormError | null> => {
+    try {
+      const res = await signIn("credentials", {
+        ...formData,
+        redirect: false,
+      });
+      if (res?.error) {
+        if (res.error === "CredentialsSignin")
+          return {
+            error: "Invalid email or password",
+          };
+        else
+          return {
+            error: "An unknown error has occurred",
+          };
+      }
+      // FIXME: This is a hack to force a revalidation of the cache
+      router.replace("/");
+      return null;
+    } catch (error) {
+      console.error("Sign in error", error);
+      return {
+        error: "An unknown error has occurred",
+      };
+    }
+  };
 
   const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
