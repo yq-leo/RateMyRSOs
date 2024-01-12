da5558dc43175161dc837eac25d6bed5a835e6f6
qiyu6 <qiyu6@illinois.edu>
2024-01-11 23:53:24 -0600

    feat: enable reuse of navbar

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <AuthProvider>
-        <NavBar />
+        <NavBar isHome={true} />
       </AuthProvider>
       {children}
     </>
